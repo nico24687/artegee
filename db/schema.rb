20a73a46f5bfc4187d5c3f7a879baa8938308a27
nico24687 <theonlynico@hotmail.com>
2018-02-19 17:07:56 -0700

    updated artists to now also have a school

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219181639) do
+ActiveRecord::Schema.define(version: 20180219235708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180219181639) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "school"
   end
 
   add_foreign_key "artworks", "users"
