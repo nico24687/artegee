6d6f49be8bd325b1380aad3a79b9f8d7351f3733
nico24687 <theonlynico@hotmail.com>
2018-02-21 20:05:11 -0700

    dealt with CSRF issue

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,5 +1,6 @@
 class OnboardingController < ApplicationController 
   layout false, only: :index
+  skip_before_action :verify_authenticity_token, only: :create
 
   def index
     @user_id = params[:user_id]
