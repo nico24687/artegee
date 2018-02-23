158177a07385a7ec149c6df72135daf375adc78b
nico24687 <theonlynico@hotmail.com>
2018-02-22 18:05:55 -0700

    fixed tests for user onboarding through typeform

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -9,6 +9,13 @@ class OnboardingController < ApplicationController
   def create
     form = OnboardingFormPresenter.new(params).form
     @user = User.find(form.user_id)
-    @user.update(first_name: form.first_name, last_name: form.last_name, school: form.school, bio: form.bio, email: form.email, location: form.location)
+    @user.update(first_name: form.first_name,
+                 last_name: form.last_name,
+                 school: form.school,
+                 bio: form.bio,
+                 email: form.email,
+                 location: form.location,
+                 style: form.style,
+                 avatar: URI.parse(form.avatar))
   end
 end
--- app/presenters/onboarding_form_presenter.rb
+++ app/presenters/onboarding_form_presenter.rb
@@ -3,6 +3,10 @@ class OnboardingFormPresenter
     @form = form 
   end
   def form 
+    
+    puts "creating form PORO..."
+    pp @form
+
     user_id = @form["form_response"]["hidden"]["user_id"]
     first_name = @form["form_response"]["answers"][0]["text"]
     last_name = @form["form_response"]["answers"][1]["text"]
@@ -11,10 +15,18 @@ class OnboardingFormPresenter
     bio = @form["form_response"]["answers"][4]["text"]
     email = @form["form_response"]["answers"][5]["text"]
     location = @form["form_response"]["answers"][6]["text"]
-    # style = @form["form_response"]["answers"][7]["choice"]["label"]
-
+    style = @form["form_response"]["answers"][7]["choice"]["label"]
+  
     #do this for all the other answers
-    OnboardingForm.new(user_id: user_id, first_name: first_name, last_name: last_name, avatar: avatar, school: school, bio: bio, email: email, location: location)
+    OnboardingForm.new(user_id: user_id, 
+                       first_name: first_name, 
+                       last_name: last_name, 
+                       avatar: avatar, 
+                       school: school, 
+                       bio: bio, 
+                       email: email, 
+                       style: style,
+                       location: location)
 
   end
 end
