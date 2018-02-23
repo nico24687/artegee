1bf3364911d2f6abec92847c61de18118481fe6b
nico24687 <theonlynico@hotmail.com>
2018-02-22 19:02:08 -0700

    Hardcode content type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" } 
-  validates_attachment :avatar, content_type: { content_type: /\Aimage\/.*\z/ }
+  validates_attachment :avatar, content_type: { content_type: "image/png" }
   has_many :artworks
 end
