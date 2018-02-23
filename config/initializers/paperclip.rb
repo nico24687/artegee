84ef3ba83c395050e21f2651c235bea377f5d060
nico24687 <theonlynico@hotmail.com>
2018-02-22 18:53:08 -0700

    disabled spoofing

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,11 @@
+require 'paperclip/media_type_spoof_detector'
+
 Paperclip::UriAdapter.register
+
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
