56e24ed06adb4f3ce31cf365e82b0981f617f5b4
nico24687 <theonlynico@hotmail.com>
2018-02-24 18:34:23 -0700

    fixed current user issue when leaving the typeform

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create 
     @user = User.create(user_params)
+    session[:user_id] = user.id
     redirect_to user_onboarding_index_path(@user)
   end
 
