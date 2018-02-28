aa6e9361e13ae6d9f339c581e3a71c17dfe5b5fe
nico24687 <theonlynico@hotmail.com>
2018-02-27 20:01:46 -0700

    dealt with issue when meetups do not have an image

--- app/presenters/meetup_presenter.rb
+++ app/presenters/meetup_presenter.rb
@@ -4,6 +4,16 @@ class MeetupPresenter
   end
   def meetup
     meetup = GetMeetupsInfoService.new.find_meetups(@location).sample(1).first
-    Meetup.new(name: meetup["name"], image: meetup["key_photo"]["highres_link"], description: meetup["description"], link: meetup["link"])
+    Meetup.new(name: meetup["name"], image: meetup_image(meetup), description: meetup["description"], link: meetup["link"])
+  end
+
+  private
+
+  def meetup_image(meetup)
+    if meetup.has_key("key_photo")
+      meetup["key_photo"]["highres_link"]
+    else
+      "https://image.ibb.co/naozDH/mountains_1x.png"
+    end
   end
 end
